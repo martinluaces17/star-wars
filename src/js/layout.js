25763ce5d86feca352c9e7cb96641ff184cfe987
Martin Luaces <martinluaces17@gmail.com>
2021-05-10 20:15:52 +0000

    sw cards

--- src/js/layout.js
+++ src/js/layout.js
@@ -9,6 +9,7 @@ import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import { Cards } from "./component/cards";
 
 //create your first component
 const Layout = () => {
@@ -21,6 +22,7 @@ const Layout = () => {
 			<BrowserRouter basename={basename}>
 				<ScrollToTop>
 					<Navbar />
+					<Cards />
 					<Switch>
 						<Route exact path="/">
 							<Home />
